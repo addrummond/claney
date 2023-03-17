6fe65ed0eca216c347615392e4d99aa978cf17b1
Alex Drummond <a.d.drummond@gmail.com>
2023-03-17 21:31:05 +0000

    update js tests

--- js/router.test.js
+++ js/router.test.js
@@ -1,7 +1,7 @@
 import { Router } from './router';
 
 // Keep in sync with value of 'routesJson' var in TestRouter in router/router_test.go
-const ROUTE_INFO = {"constantPortionNGroups":34,"constantPortionRegexp":"^(?:(?:\\/+(users)\\/*)(?:|\\/+(?:(\\.)\\/*)|(?:\\/+[^\\/?#]+\\/+(home)\\/*)|(?:\\/+[^\\/?#]+\\/+(profile)\\/*)|(?:\\/+[^\\/?#]+\\/+(orders)\\/*)(?:|(?:\\/+(display)\\/+[^\\/?#]+\\/*)))|(?:\\/+(managers)\\/*)(?:|(?:\\/+[^\\/?#]+\\/+(home)\\/*)|(?:\\/+[^\\/?#]+\\/+(profile)\\/*)|(?:\\/+[^\\/?#]+\\/+(stats)\\/*)|(?:\\/+(orders)\\/+[^\\/?#]+\\/+[^\\/?#]+\\/+(theorder)\\/*)|(?:\\/+(foo)\\/+(goo)\\/+(bar)\\/+[^\\/?#]+\\/*)|(?:\\/+(foo)\\/+(bar)\\/+[^\\/?#]+\\/*)|(?:\\/+(routeending\\\\withbackslash\\\\)\\/*))|(?:\\/+(users)\\/*)(?:|\\/+(?:(foo)\\/*)|(?:\\/+(x)\\/+(y)\\/+(z)\\/+(k)\\/*))|(?:\\/*)(?:|(?:\\/+(foo)\\/+(bar)\\/+[^\\/?#]+\\/*)|(?:\\/+(foobar)\\/+[^\\/?#]+\\/*)|(?:\\/+(f)\\/+(oo)\\/+(bar)\\/+[^\\/?#]+\\/*)|(?:\\/+(fooba)\\/+(r)\\/+[^\\/?#]+\\/*)|(?:\\/+(f)\\/+(oobar)\\/+[^\\/?#]+\\/*)))(?:\\?[^#]*)?(?:#.*)?$","families":{"usersxyzk":{"matchRegexp":"^(?:(\\/+users\\/*\\/+x\\/+y\\/+z\\/+k\\/*))(\\?[^#]*)?(#.*)?$","nLevels":1,"nonparamGroupNumbers":[1],"members":[{"name":"users/another","paramGroupNumbers":{},"tags":[]}]},"usershome":{"matchRegexp":"^(?:(\\/+users\\/*\\/+([^\\/?#]+)\\/+home\\/*))(\\?[^#]*)?(#.*)?$","nLevels":1,"nonparamGroupNumbers":[1],"members":[{"name":"users/home","paramGroupNumbers":{"user_id":2},"tags":[]}]},"managersorderstheorder":{"matchRegexp":"^(?:(\\/+managers\\/*\\/+orders\\/+([^\\/?#]+)\\/+([^\\/?#]+)\\/+theorder\\/*))(\\?[^#]*)?(#.*)?$","nLevels":1,"nonparamGroupNumbers":[1],"members":[{"name":"managers/orders","paramGroupNumbers":{"user_id":2,"o rder_}\\id":3},"tags":["a tag to maybe inherit","baz"]}]},"managersrouteending\\withbackslash\\":{"matchRegexp":"^(?:(\\/+managers\\/*\\/+routeending\\\\withbackslash\\\\\\/*))(\\?[^#]*)?(#.*)?$","nLevels":1,"nonparamGroupNumbers":[1],"members":[{"name":"managers/backslash","paramGroupNumbers":{},"tags":[]}]},"usersprofile":{"matchRegexp":"^(?:(\\/+users\\/*\\/+([^\\/?#]+)\\/+profile\\/*))(\\?[^#]*)?(#.*)?$","nLevels":1,"nonparamGroupNumbers":[1],"members":[{"name":"users/profile","paramGroupNumbers":{"user_id":2},"tags":[]}]},"managershome":{"matchRegexp":"^(?:(\\/+managers\\/*\\/+([^\\/?#]+)\\/+home\\/*))(\\?[^#]*)?(#.*)?$","nLevels":1,"nonparamGroupNumbers":[1],"members":[{"name":"managers/home","paramGroupNumbers":{"manager_id":2},"tags":[]}]},"managersprofile":{"matchRegexp":"^(?:(\\/+managers\\/*\\/+([^\\/?#]+)\\/+profile\\/*))(\\?[^#]*)?(#.*)?$","nLevels":1,"nonparamGroupNumbers":[1],"members":[{"name":"managers/profile","paramGroupNumbers":{"manager_id":2},"tags":[]}]},"usersfoo":{"matchRegexp":"^(?:(\\/+users\\/*\\/+foo\\/*))(\\?[^#]*)?(#.*)?$","nLevels":1,"nonparamGroupNumbers":[1],"members":[{"name":"users/foo","paramGroupNumbers":{},"tags":[]}]},"foobar":{"matchRegexp":"^(?:(((\\/*\\/+foo\\/+bar\\/+([^\\/?#]+)\\/*)|(\\/*\\/+foobar\\/+([^\\/?#]+)\\/*))|((\\/*\\/+f\\/+oo\\/+bar\\/+([^\\/?#]+)\\/*)|(\\/*\\/+fooba\\/+r\\/+([^\\/?#]+)\\/*)))|(((\\/*\\/+f\\/+oobar\\/+([^\\/?#]+)\\/*))))(\\?[^#]*)?(#.*)?$","nLevels":3,"nonparamGroupNumbers":[1,2,3,5,7,8,10,12,13,14],"members":[{"name":"dupl/a","paramGroupNumbers":{"param":4},"tags":[]},{"name":"dupl/b","paramGroupNumbers":{"param":6},"tags":[]},{"name":"dupl/c","paramGroupNumbers":{"param":9},"tags":[]},{"name":"dupl/d","paramGroupNumbers":{"param":11},"tags":[]},{"name":"dupl/e","paramGroupNumbers":{"param":15},"tags":[]}]},"managersstats":{"matchRegexp":"^(?:(\\/+managers\\/*\\/+([^\\/?#]+)\\/+stats\\/*))(\\?[^#]*)?(#.*)?$","nLevels":1,"nonparamGroupNumbers":[1],"members":[{"name":"managers/stats","paramGroupNumbers":{"manager_id":2},"tags":["amp","bar","foo"]}]},"users":{"matchRegexp":"^(?:(\\/+users\\/*))(\\?[^#]*)?(#.*)?$","nLevels":1,"nonparamGroupNumbers":[1],"members":[{"name":"users","paramGroupNumbers":{},"tags":[]}]},"users.":{"matchRegexp":"^(?:(\\/+users\\/*\\/+\\.\\/*))(\\?[^#]*)?(#.*)?$","nLevels":1,"nonparamGroupNumbers":[1],"members":[{"name":"users/dot","paramGroupNumbers":{},"tags":[]}]},"usersordersdisplay":{"matchRegexp":"^(?:(\\/+users\\/*\\/+([^\\/?#]+)\\/+orders\\/*\\/+display\\/+([^\\/?#]+)\\/*))(\\?[^#]*)?(#.*)?$","nLevels":1,"nonparamGroupNumbers":[1],"members":[{"name":"users/orders/order","paramGroupNumbers":{"user_id":2,"order_id":3},"tags":[]}]},"managersfoogoobar":{"matchRegexp":"^(?:(\\/+managers\\/*\\/+foo\\/+goo\\/+bar\\/+([^\\/?#]+)\\/*))(\\?[^#]*)?(#.*)?$","nLevels":1,"nonparamGroupNumbers":[1],"members":[{"name":"managers/test1","paramGroupNumbers":{"maguffin":2},"tags":[]}]},"managersfoobar":{"matchRegexp":"^(?:(\\/+managers\\/*\\/+foo\\/+bar\\/+([^\\/?#]+)\\/*))(\\?[^#]*)?(#.*)?$","nLevels":1,"nonparamGroupNumbers":[1],"members":[{"name":"managers/test2","paramGroupNumbers":{"maguffin":2},"tags":["a tag to maybe inherit"]}]}}};
+const ROUTE_INFO = {"constantPortionNGroups":77,"constantPortionRegexp":"^(?:(?:\\/+(users))(?:|(?:(\\/)\\/*(\\.)\\/*)|(?:(\\/)\\/*[^\\/?#]+(\\/)\\/*(home)\\/*)|(?:(\\/)\\/*[^\\/?#]+(\\/)\\/*(profile)\\/*)|(?:(\\/)\\/*[^\\/?#]+(\\/)\\/*(orders))(?:(?:(\\/)\\/*(display)(\\/)\\/*[^\\/?#]+\\/*)))|(?:\\/+(managers))(?:\\/+|(?:(\\/)\\/*[^\\/?#]+(\\/)\\/*(home)\\/*)|(?:(\\/)\\/*[^\\/?#]+(\\/)\\/*(profile)\\/*)|(?:(\\/)\\/*[^\\/?#]+(\\/)\\/*(stats)\\/*)|(?:(\\/)\\/*(orders)(\\/)\\/*[^\\/?#]+(\\/)\\/*[^\\/?#]+(\\/)\\/*(theorder)\\/*)|(?:(\\/)\\/*(foo)(\\/)\\/*(goo)(\\/)\\/*(bar)(\\/)\\/*[^\\/?#]+\\/*)|(?:(\\/)\\/*(foo)(\\/)\\/*(bar)(\\/)\\/*[^\\/?#]+\\/*)|(?:(\\/)\\/*(routeending\\\\withbackslash\\\\)\\/*)|(?:(\\/)\\/*(foo)(\\/)\\/*(blobby)(\\/)\\/*\\/*[^\\/?#][^?#]*\\/*)|(?:(\\/)\\/*(fooo)(\\/)\\/*(blobby)(\\/)\\/*\\/*[^\\/?#][^?#]*?(\\/)\\/*(more)\\/*))|(?:\\/+(users))(?:(?:(\\/)\\/*(foo)\\/*)|(?:(\\/)\\/*(x)(\\/)\\/*(y)(\\/)\\/*(z)(\\/)\\/*(k)\\/*))|(?:\\/+)(?:(?:(foo\\.x)-?[0-9]+(xxxx)\\/*)|(?:(foo\\.xx)-?[0-9]+(xxx)\\/*)|(?:(foo\\.xxx)-?[0-9]+(xx)\\/*)|(?:(foo\\.xxxx)-?[0-9]+(x)\\/*)|(?:(foo\\.xxxxx)-?[0-9]+\\/*)))(?:\\?[^#]*)?(?:#.*)?$","families":{"foo.xxxxx":{"matchRegexp":"^(?:(((\\/+foo\\.x(-?[0-9]+)xxxx\\/*)|(\\/+foo\\.xx(-?[0-9]+)xxx\\/*))|((\\/+foo\\.xxx(-?[0-9]+)xx\\/*)|(\\/+foo\\.xxxx(-?[0-9]+)x\\/*)))|(((\\/+foo\\.xxxxx(-?[0-9]+)\\/*))))(\\?[^#]*)?(#.*)?$","nLevels":3,"nonparamGroupNumbers":[1,2,3,5,7,8,10,12,13,14],"members":[{"name":"dupl/a","paramGroupNumbers":{"param":4},"tags":[],"methods":["GET"]},{"name":"dupl/b","paramGroupNumbers":{"param":6},"tags":[],"methods":["GET"]},{"name":"dupl/c","paramGroupNumbers":{"param":9},"tags":[],"methods":["GET"]},{"name":"dupl/d","paramGroupNumbers":{"param":11},"tags":[],"methods":["GET"]},{"name":"dupl/e","paramGroupNumbers":{"param":15},"tags":[],"methods":["GET"]}]},"managers":{"matchRegexp":"^(?:(\\/+managers\\/+))(\\?[^#]*)?(#.*)?$","nLevels":1,"nonparamGroupNumbers":[1],"members":[{"name":"managers","paramGroupNumbers":{},"tags":["a tag to inherit"],"methods":["GET"]}]},"managers//home":{"matchRegexp":"^(?:(\\/+managers\\/+([^\\/?#]+)\\/+home\\/*))(\\?[^#]*)?(#.*)?$","nLevels":1,"nonparamGroupNumbers":[1],"members":[{"name":"managers/home","paramGroupNumbers":{"manager_id":2},"tags":["a tag to inherit"],"methods":["GET"]}]},"managers//profile":{"matchRegexp":"^(?:(\\/+managers\\/+([^\\/?#]+)\\/+profile\\/*))(\\?[^#]*)?(#.*)?$","nLevels":1,"nonparamGroupNumbers":[1],"members":[{"name":"managers/profile","paramGroupNumbers":{"manager_id":2},"tags":["a tag to inherit"],"methods":["GET"]}]},"managers//stats":{"matchRegexp":"^(?:(\\/+managers\\/+([^\\/?#]+)\\/+stats\\/*))(\\?[^#]*)?(#.*)?$","nLevels":1,"nonparamGroupNumbers":[1],"members":[{"name":"managers/stats","paramGroupNumbers":{"manager_id":2},"tags":["a tag to inherit","amp","bar","foo"],"methods":["POST","PUT"]}]},"managers/foo/bar/":{"matchRegexp":"^(?:(\\/+managers\\/+foo\\/+bar\\/+([^\\/?#]+)\\/*))(\\?[^#]*)?(#.*)?$","nLevels":1,"nonparamGroupNumbers":[1],"members":[{"name":"managers/test2","paramGroupNumbers":{"maguffin":2},"tags":["a tag to inherit"],"methods":["GET"]}]},"managers/foo/blobby/":{"matchRegexp":"^(?:(\\/+managers\\/+foo\\/+blobby\\/+(\\/*[^\\/?#][^?#]*)\\/*))(\\?[^#]*)?(#.*)?$","nLevels":1,"nonparamGroupNumbers":[1],"members":[{"name":"managers/resty","paramGroupNumbers":{"rest":2},"tags":["a tag to inherit"],"methods":["GET"]}]},"managers/foo/goo/bar/":{"matchRegexp":"^(?:(\\/+managers\\/+foo\\/+goo\\/+bar\\/+([^\\/?#]+)\\/*))(\\?[^#]*)?(#.*)?$","nLevels":1,"nonparamGroupNumbers":[1],"members":[{"name":"managers/test1","paramGroupNumbers":{"maguffin":2},"tags":["a tag to inherit"],"methods":["POST"]}]},"managers/fooo/blobby//more":{"matchRegexp":"^(?:(\\/+managers\\/+fooo\\/+blobby\\/+(\\/*[^\\/?#][^?#]*?)\\/+more\\/*))(\\?[^#]*)?(#.*)?$","nLevels":1,"nonparamGroupNumbers":[1],"members":[{"name":"managers/resty","paramGroupNumbers":{"rest":2},"tags":["a tag to inherit"],"methods":["GET"]}]},"managers/orders///theorder":{"matchRegexp":"^(?:(\\/+managers\\/+orders\\/+([^\\/?#]+)\\/+([^\\/?#]+)\\/+theorder\\/*))(\\?[^#]*)?(#.*)?$","nLevels":1,"nonparamGroupNumbers":[1],"members":[{"name":"managers/orders","paramGroupNumbers":{"user_id":2,"o rder_}\\id":3},"tags":["a tag to inherit","baz"],"methods":["GET"]}]},"managers/routeending\\withbackslash\\":{"matchRegexp":"^(?:(\\/+managers\\/+routeending\\\\withbackslash\\\\\\/*))(\\?[^#]*)?(#.*)?$","nLevels":1,"nonparamGroupNumbers":[1],"members":[{"name":"managers/backslash","paramGroupNumbers":{},"tags":["a tag to inherit"],"methods":["GET"]}]},"users":{"matchRegexp":"^(?:(\\/+users))(\\?[^#]*)?(#.*)?$","nLevels":1,"nonparamGroupNumbers":[1],"members":[{"name":"users","paramGroupNumbers":{},"tags":[],"methods":["GET"]}]},"users/.":{"matchRegexp":"^(?:(\\/+users\\/+\\.\\/*))(\\?[^#]*)?(#.*)?$","nLevels":1,"nonparamGroupNumbers":[1],"members":[{"name":"users/dot","paramGroupNumbers":{},"tags":[],"methods":["GET"]}]},"users//home":{"matchRegexp":"^(?:(\\/+users\\/+([^\\/?#]+)\\/+home\\/*))(\\?[^#]*)?(#.*)?$","nLevels":1,"nonparamGroupNumbers":[1],"members":[{"name":"users/home","paramGroupNumbers":{"user_id":2},"tags":[],"methods":["GET"]}]},"users//orders/display/":{"matchRegexp":"^(?:(\\/+users\\/+([^\\/?#]+)\\/+orders\\/+display\\/+([^\\/?#]+)\\/*))(\\?[^#]*)?(#.*)?$","nLevels":1,"nonparamGroupNumbers":[1],"members":[{"name":"users/orders/order","paramGroupNumbers":{"user_id":2,"order_id":3},"tags":[],"methods":["GET"]}]},"users//profile":{"matchRegexp":"^(?:(\\/+users\\/+([^\\/?#]+)\\/+profile\\/*))(\\?[^#]*)?(#.*)?$","nLevels":1,"nonparamGroupNumbers":[1],"members":[{"name":"users/profile","paramGroupNumbers":{"user_id":2},"tags":[],"methods":["GET"]}]},"users/foo":{"matchRegexp":"^(?:(\\/+users\\/+foo\\/*))(\\?[^#]*)?(#.*)?$","nLevels":1,"nonparamGroupNumbers":[1],"members":[{"name":"users/foo","paramGroupNumbers":{},"tags":[],"methods":["GET"]}]},"users/x/y/z/k":{"matchRegexp":"^(?:(\\/+users\\/+x\\/+y\\/+z\\/+k\\/*))(\\?[^#]*)?(#.*)?$","nLevels":1,"nonparamGroupNumbers":[1],"members":[{"name":"users/another","paramGroupNumbers":{},"tags":[],"methods":["GET"]}]}}};
 const router = new Router(ROUTE_INFO);
 
 test('yields expected results for various routes', () => {
@@ -9,6 +9,7 @@ test('yields expected results for various routes', () => {
   expect(router.route("")).toEqual(null);
   expect(router.route("/users")).toEqual({
     name: "users",
+    methods: ["GET"],
     params: {},
     query: "",
     anchor: "",
@@ -16,6 +17,7 @@ test('yields expected results for various routes', () => {
   });
   expect(router.route("/users/.")).toEqual({
     name: "users/dot",
+    methods: ["GET"],
     params: {},
     query: "",
     anchor: "",
@@ -23,6 +25,7 @@ test('yields expected results for various routes', () => {
   });
   expect(router.route("/users/123/home//")).toEqual({
     name: "users/home",
+    methods: ["GET"],
     params: {user_id: "123"},
     query: "",
     anchor: "",
@@ -30,6 +33,7 @@ test('yields expected results for various routes', () => {
   });
   expect(router.route("/users//123/profile")).toEqual({
     name: "users/profile",
+    methods: ["GET"],
     params: {user_id: "123"},
     query: "",
     anchor: "",
@@ -38,6 +42,7 @@ test('yields expected results for various routes', () => {
   expect(router.route("/users/123/orders")).toEqual(null);
   expect(router.route("/users/123/orders/display/456")).toEqual({
     name: "users/orders/order",
+    methods: ["GET"],
     params: {user_id: "123", order_id: "456"},
     query: "",
     anchor: "",
@@ -47,77 +52,88 @@ test('yields expected results for various routes', () => {
   expect(router.route("managers")).toEqual(null);
   expect(router.route("/managers//123/home/")).toEqual({
     name: "managers/home",
+    methods: ["GET"],
     params: {manager_id: "123"},
     query: "",
     anchor: "",
-    tags: []
+    tags: ["a tag to inherit"]
   });
   expect(router.route("/managers/123/stats///")).toEqual({
     name: "managers/stats",
+    methods: ["POST", "PUT"],
     params: {manager_id: "123"},
     query: "",
     anchor: "",
-    tags: ["amp", "bar", "foo"]
+    tags: ["a tag to inherit", "amp", "bar", "foo"]
   });
   expect(router.route("/managers/orders/123/456/theorder")).toEqual({
     name: "managers/orders",
+    methods: ["GET"],
     params: {user_id: "123", "o rder_}\\id": "456"},
     query: "",
     anchor: "",
-    tags: ["a tag to maybe inherit", "baz"]
+    tags: ["a tag to inherit", "baz"]
   });
   expect(router.route("/managers/foo/goo/bar/123")).toEqual({
     name: "managers/test1",
+    methods: ["POST"],
     params: {maguffin: "123"},
     query: "",
     anchor: "",
-    tags: []
+    tags: ["a tag to inherit"]
   });
   expect(router.route("/managers/foo/bar/123")).toEqual({
     name: "managers/test2",
+    methods: ["GET"],
     params: {maguffin: "123"},
     query: "",
     anchor: "",
-    tags: ["a tag to maybe inherit"]
+    tags: ["a tag to inherit"]
   });
   expect(router.route("/managers/routeending\\withbackslash\\")).toEqual({
     name: "managers/backslash",
+    methods: ["GET"],
     params: {},
     query: "",
     anchor: "",
-    tags: []
+    tags: ["a tag to inherit"]
   });
 
-  expect(router.route("/foo//bar/123")).toEqual({
+  expect(router.route("/foo.x123xxxx")).toEqual({
     name: "dupl/a",
     params: {"param": "123"},
+    methods: ["GET"],
     query: "",
     anchor: "",
     tags: []
   });
-  expect(router.route("/foobar/123#foo")).toEqual({
+  expect(router.route("/foo.xx123xxx#foo")).toEqual({
     name: "dupl/b",
+    methods: ["GET"],
     params: {"param": "123"},
     query: "",
     anchor: "#foo",
     tags: []
   });
-  expect(router.route("/f/oo/bar/123")).toEqual({
+  expect(router.route("/foo.xxx123xx")).toEqual({
     name: "dupl/c",
+    methods: ["GET"],
     params: {"param": "123"},
     query: "",
     anchor: "",
     tags: []
   });
-  expect(router.route("/fooba/r/123")).toEqual({
+  expect(router.route("/foo.xxxx123x")).toEqual({
     name: "dupl/d",
+    methods: ["GET"],
     params: {"param": "123"},
     query: "",
     anchor: "",
     tags: []
   });
-  expect(router.route("/f/oobar/123")).toEqual({
+  expect(router.route("/foo.xxxxx123")).toEqual({
     name: "dupl/e",
+    methods: ["GET"],
     params: {"param": "123"},
     query: "",
     anchor: "",
@@ -126,27 +142,31 @@ test('yields expected results for various routes', () => {
 
   expect(router.route("/managers/123/profile?with=aquery&string=bar")).toEqual({
     name: "managers/profile",
+    methods: ["GET"],
     params: {"manager_id": "123"},
     query: "?with=aquery&string=bar",
     anchor: "",
-    tags: []
+    tags: ["a tag to inherit"]
   });
   expect(router.route("/managers/foo/bar/123?with=aquery&string=foo")).toEqual({
     name: "managers/test2",
+    methods: ["GET"],
     params: {"maguffin": "123"},
     query: "?with=aquery&string=foo",
     anchor: "",
-    tags: ["a tag to maybe inherit"]
+    tags: ["a tag to inherit"]
   });
-  expect(router.route("/fooba/r/123#foo?q=a&boo=c")).toEqual({
+  expect(router.route("/foo.xxxx123x#foo?q=a&boo=c")).toEqual({
     name: "dupl/d",
+    methods: ["GET"],
     params: {"param": "123"},
     query: "",
     anchor: "#foo?q=a&boo=c",
     tags: []
   });
-  expect(router.route("/f/oobar/123?q=a#foo")).toEqual({
+  expect(router.route("/foo.xxxxx123?q=a#foo")).toEqual({
     name: "dupl/e",
+    methods: ["GET"],
     params: {"param": "123"},
     query: "?q=a",
     anchor: "#foo",
