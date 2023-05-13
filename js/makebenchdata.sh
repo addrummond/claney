67d33065a34c5b19a3baf199d8369dc8f1c3188c
Alex Drummond <a.d.drummond@gmail.com>
2023-05-13 16:51:15 +0100

    add script for generating bench data

---
+++ js/makebenchdata.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+n="$1"
+out="/$TMPDIR/makebenchdataouttmpfile"
+
+rm -f $out
+
+i=0
+while [ $i -le $n ]; do
+  echo "foo${i} /${i}foo" >> $out
+  i=$(( i + 1 ))
+done
+
+d=$(basename $(pwd))
+cd ..
+go run main.go -input $out
